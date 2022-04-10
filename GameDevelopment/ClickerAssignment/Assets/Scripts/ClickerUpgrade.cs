b63df9f4a73cbd0ecbd3d94463f5d2b8450177f0
Lunar <lunarblacksmith@gmail.com>
2022-04-10 20:01:39 +1000

    Updated UI to display counters and got coroutine timer to work
    - added 3 counter UI
    - got population decreasing in intervals
    - message to the user displays when upgrades are purchased.

--- GameDevelopment/ClickerAssignment/Assets/Scripts/ClickerUpgrade.cs
+++ GameDevelopment/ClickerAssignment/Assets/Scripts/ClickerUpgrade.cs
@@ -5,11 +5,11 @@ using UnityEngine.UI;
 
 public class ClickerUpgrade : MonoBehaviour
 {
-    [Tooltip("This is the Text relating to this specific Game Object.")]
+    //[Tooltip("This is the Text relating to this specific Game Object.")]
     public Text costText;
-    int _cost = 100;
-    int _clickerGrowthVar = 1;
-    int _costGrowthVar = 1;
+    public GameManager gameManager;
+    int _cost = 100; //initial cost of a cost upgrade
+    int _clickerGrowthVar = 1, _costGrowthVar = 1; //despite what VS thinks, these are manipulated through properties in local methods
     public int Cost 
     {
         get { return _cost; }
@@ -17,6 +17,13 @@ public class ClickerUpgrade : MonoBehaviour
     }
     public int ClickerGrowthVar { get; private set; }
     public int CostGrowthVar { get; private set; }
+
+    public void Start()
+    {
+        Debug.Log($"Cost Text for Clicker: {costText.text}");
+        gameManager.UpdateTextField(2); //Updates the text displaying how many inhabitants per click the player has when game starts
+        costText.text = $"UPGRADE POP/CLICK\n{Cost.ToString("N0")} Population"; //update button text for upgrade
+    } 
 
     /// <summary>
     /// Decrease current inhabitants amount by the cost value. If cost is greater than population, prints a console message.
@@ -27,25 +34,20 @@ public class ClickerUpgrade : MonoBehaviour
         {
             //subtract cost value from current population value
             GameManager.inhabitants -= Cost;
-            Debug.Log("You purchased an upgrade!");
             //increase the amount of inhabitants per click by 1 times our clicker growth variable
             ClickHandler.ipclick += 2 * ClickerGrowthVar;
-            //increase the cost of next purchase by 100
+            gameManager.SendMessageToUser($"You now have {ClickHandler.ipclick} inhabitants per click!", 2f);
+            //increase the cost of next purchase using a curved algorithm
             Cost += 110 * CostGrowthVar;
             //increment both growth variables by 1
             ++ClickerGrowthVar;
             ++CostGrowthVar;
-            //if the costText reference isn't null
-            if (costText)
-            {
-                //Show UPGRADE [cost amount formatted to separate by comma in thousands] Population
-                costText.text = $"UPGRADE\n{Cost.ToString("N0")} Population";
-            }
+            costText.text = $"UPGRADE POP/CLICK\n{Cost.ToString("N0")} Population";
         }
         else
-        {
-            Debug.Log("You need more inhabitants before you can afford this upgrade.");
-        }
+        { gameManager.SendMessageToUser($"You need more inhabitants before you can afford this upgrade.", 2f); }
+        //don't need to call this every frame since it's only affected when a new clicker upgrade is purchased
+        gameManager.UpdateTextField(2); 
     }
 
     #region Dev Cheats
